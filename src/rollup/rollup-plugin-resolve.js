166735ba04463071f0eeec9ec3580ab02c74ab0c
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-09-09 20:41:30 +0200

    Generalized rollup resolve plugin

--- src/rollup/rollup-plugin-resolve.js
+++ src/rollup/rollup-plugin-resolve.js
@@ -1,16 +1,26 @@
 var Module = require('module');
 var path = require('path');
 
+var DOT = '.'.charCodeAt(0);
+
 // rather dangerous iplementation of nodejs resolve
 // using node's private API
-export default function resolve(opts) {
+export default function resolve() {
   return {
     name: "resolve",
     resolveId: function(importee, importer) {
       // skip relative paths
-      if (!importer || !importee || importee[0] === '.') return null;
+      if (!importer || !importee/* || importee[0] === '.'*/) return null;
+      if (importee.charCodeAt(0) === DOT) {
+        try {
+          var id = path.join(path.dirname(importer), importee);
+          return require.resolve(id);
+        } catch (err) {
+          return null;
+        }
+      }
       // skip strange importees
-      if (!/^\w/.exec(importee)) return null;
+      if (!/^[\w]/.exec(importee)) return null;
       // console.log('## resolving %s from %s', importee, importer);
       var dirname = path.dirname(importer);
       var paths = Module._nodeModulePaths(dirname);
