9027eaa40d02557a2a6bd32b6fd028f15cb7c407
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-01-19 09:58:26 +0100

    Fix resolve plugin.

--- src/rollup/rollup-plugin-resolve.js
+++ src/rollup/rollup-plugin-resolve.js
@@ -1,9 +1,9 @@
-var Module = require('module')
-var path = require('path')
-var DOT = '.'.charCodeAt(0)
-var fs = require('fs')
+import { isAbsolute } from '../fileUtils'
+const Module = require('module')
+const path = require('path')
+const fs = require('fs')
 
-const ZERO = "\0".charCodeAt(0)
+const DOT = '.'.charCodeAt(0)
 
 // rather dangerous iplementation of nodejs resolve
 // using node's private API
@@ -19,21 +19,26 @@ export default function resolve(opts) {
   return {
     name: "resolve",
     resolveId: function(importee, importer) {
-      // skip relative paths
-      if (!importer || !importee/* || importee[0] === '.'*/) return null
-      // skip internal imports
-      if (importee.charCodeAt(0) === ZERO) return importee
+      // console.log('## resolve(%s, %s)', importee, importer)
+      if (/\0/.test(importee)) return null // ignore IDs with null character, these belong to other plugins
+      // disregard entry module
+      if (!importer) return null
+      if (!isAbsolute(importer)) return null
       // process relative imports
       if (importee.charCodeAt(0) === DOT) {
         try {
+          // console.log('.... resolving relatively: %s from %s', importee, importer)
           return require.resolve(path.join(path.dirname(importer), importee))
         } catch (err) {
           return null
         }
       }
       // replace alias
-      if (alias[importee]) importee = alias[importee]
-
+      // TODO: shouldn't this be another plugin?
+      if (alias[importee]) {
+        // console.log('.... using alias: %s -> %s', importee, alias[importee])
+        return alias[importee]
+      }
       // console.log('## resolving %s from %s', importee, importer)
       let dirname = path.dirname(importer)
       let paths = Module._nodeModulePaths(dirname)
@@ -49,14 +54,14 @@ export default function resolve(opts) {
           let entry = pkg['jsnext:main']
           if (entry) {
             let p = path.join(path.dirname(pkgPathAbs), entry)
-            // console.log('resolve: resolved via package jsnext:main', p)
+            // console.log('.... resolved via package jsnext:main', p)
             return p
           }
         }
       }
       let p = Module._findPath(importee, paths, false) || null
       if (p) {
-        // console.log('resolve: ', p)
+        // console.log('.... resolved', p)
         return p
       }
     }
