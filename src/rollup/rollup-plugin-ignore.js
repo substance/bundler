e95aaf6c913155e21e91f420cfe090f35eef9ba7
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-01-19 09:58:36 +0100

    Improved ignore plugin

--- src/rollup/rollup-plugin-ignore.js
+++ src/rollup/rollup-plugin-ignore.js
@@ -1,20 +1,20 @@
+const EMPTY_ID = "\0ignore:empty.js"
+const EMPTY_CODE = "export default {}"
+
 export default function ignore(opts) {
-  // TODO: maybe we want something more sophisticated
-  // than just an exact match
   const ignore = (opts.ignore || [])
   return {
     name: "ignore",
-    resolveId: function(importee, importer) {
-      for (var i = 0; i < ignore.length; i++) {
-        if(ignore[i] === importee) {
-          // console.info('.. ignoring module %s', importee)
-          return '__empty__'
-        }
+    resolveId: function(importee, importer) { // eslint-disable-line no-unused-vars
+      if (importee === EMPTY_ID) return importee
+      if (ignore.indexOf(importee) > -1) {
+        // console.info('## ignoring module %s', importee)
+        return EMPTY_ID
       }
     },
     load: function(id) {
-      if (id === '__empty__') {
-        return "export default {}"
+      if (id === EMPTY_ID) {
+        return EMPTY_CODE
       }
     }
   }
