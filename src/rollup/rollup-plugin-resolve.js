2b3c6c1234c090eb5ba72d54fbd4814d2ac92832
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-11-26 11:04:25 +0100

    Improve debug messages of rollup resolver plugin.

--- src/rollup/rollup-plugin-resolve.js
+++ src/rollup/rollup-plugin-resolve.js
@@ -1,5 +1,7 @@
 import { isAbsolute } from '../fileUtils'
-import { minimatch } from '../vendor'
+import { minimatch, debug as _debug, colors } from '../vendor'
+
+const debug = _debug('bundler:rollup:resolve')
 
 const Module = require('module')
 const path = require('path')
@@ -14,12 +16,16 @@ const DOT = '.'.charCodeAt(0)
 export default function resolve(opts) {
   opts = opts || {}
   const alias = opts.alias || {}
+  if (opts.alias) {
+    debug('Using aliases: %s', JSON.stringify(opts.alias, 0, 2))
+  }
   const ignore = (opts.ignore || []).map((pattern) => {
     // is glob pattern?
     if (pattern.indexOf('*') > -1) {
       return (f) => {
         let result = minimatch(f, pattern) || minimatch(path.basename(f), pattern)
-        // console.log('#### %s?', pattern, f, result)
+        // TODO: improve debug msg
+        // debug('#### %s?', pattern, f, result)
         return result
       }
     } else {
@@ -35,11 +41,11 @@ export default function resolve(opts) {
   }
   return {
     name: "resolve",
-    resolveId: function(importee, importer) {
-      const DEBUG = false
+    resolveId: function resolveId(importee, importer) {
+      const DEBUG = debug.enabled
       // const DEBUG = importee.indexOf('domutils') >= 0
+      if (DEBUG) debug('resolving %s from %s', importee, importer)
 
-      if (DEBUG) console.info('### resolve', importee, importer)
       if (importee === EMPTY_ID) return EMPTY_ID
       // ignore IDs with null character, these belong to other plugins
       if (/\0/.test(importee)) return null
@@ -63,46 +69,72 @@ export default function resolve(opts) {
           }
         }
       }
+      // Note about aliases:
+      // This is a custom feature of our
+      // rollup resolve plugin, that allows us
+      // to redirect certain imports to other
+      // files on the disk. E.g. this is important
+      // in a mixed CommonJS and ES6 scenario,
+      // where it can be easier to use browserify
+      // to bundle subtrees, and let rollup do the rest.
+
       // process relative imports
       if (importee.charCodeAt(0) === DOT) {
-        if (DEBUG) console.info('.... resolving relatively: %s from %s', importee, importer)
+        if (DEBUG) debug('.. resolving relatively: %s from %s', importee, importer)
         // first try with extension added
         let _importee = path.join(path.dirname(importer), _withExtension(importee))
-        if (DEBUG) console.info('...... trying: %s', _importee)
+        // redirecting if there is an alias
+        if (alias[_importee]) {
+          if (DEBUG) debug('.. '+colors.green('using ALIAS')+': %s -> %s', _importee, alias[_importee])
+          return resolveId(alias[_importee], importer)
+        }
         try {
+          if (DEBUG) debug('.. trying: %s', _importee)
           let p = require.resolve(_importee)
-          if (DEBUG) console.info('.... resolved', p)
+          if (DEBUG) debug('.. found', p)
           return _withExtension(p)
         } catch (err) { /* nothing */ }
         // then try as it is written originally
         _importee = path.join(path.dirname(importer), importee)
-        if (DEBUG) console.info('...... trying: %s', _importee)
+        // redirecting if there is an alias
+        if (alias[_importee]) {
+          if (DEBUG) debug('.. '+colors.green('using ALIAS')+': %s -> %s', _importee, alias[_importee])
+          return resolveId(alias[_importee], importer)
+        }
         try {
+          if (DEBUG) debug('.. trying: %s', _importee)
           let p = require.resolve(_importee)
-          if (DEBUG) console.info('.... resolved', p)
+          if (DEBUG) debug('.. found', p)
           return _withExtension(p)
         } catch (err) { /* nothing */ }
         // finally try index.js
         _importee = path.join(path.dirname(importer), importee, 'index.js')
-        if (DEBUG) console.info('...... trying: %s', _importee)
+        // redirecting if there is an alias
+        if (alias[_importee]) {
+          if (DEBUG) debug('.. '+colors.green('using ALIAS')+': %s -> %s', _importee, alias[_importee])
+          return resolveId(alias[_importee], importer)
+        }
         try {
+          if (DEBUG) debug('.. trying: %s', _importee)
           let p = require.resolve(_importee)
-          if (DEBUG) console.info('.... resolved', p)
+          if (DEBUG) debug('.. found', p)
           return _withExtension(p)
         } catch (err) { /* nothing */ }
         // if non of the above works, return null which means a failed lookup
+        if (DEBUG) debug('.. not found')
         return null
       }
+
+      // aliases for module imports or absolute file paths
       if (alias[importee]) {
-        if (DEBUG) console.info('.... using alias: %s -> %s', importee, alias[importee])
+        if (DEBUG) debug('.. '+colors.green('using ALIAS')+': %s -> %s', importee, alias[importee])
         importee = alias[importee]
       }
 
-      if (DEBUG) console.info('## resolving %s from %s', importee, importer)
       let dirname = path.dirname(importer)
       let paths = Module._nodeModulePaths(dirname)
       // try to find it first before doing something special
-      if (DEBUG) console.info('## TRYING to _findPath', importee)
+      if (DEBUG) debug('.. trying _findPath')
       // then try to look for a node_module
       // NOTE: jsnext:main is now the default for modules
       // if you want to support legacy modules you need to provide an array of cjs modules
@@ -116,14 +148,14 @@ export default function resolve(opts) {
           let entry = pkg['jsnext:main']
           if (entry) {
             let p = path.join(path.dirname(pkgPathAbs), entry)
-            if (DEBUG) console.info('.... resolved via package jsnext:main', p)
+            if (DEBUG) debug('.. found via package jsnext:main: %s', p)
             return _withExtension(p)
           }
         }
       }
       let p = Module._findPath(importee, paths, false) || null
       if (p) {
-        if (DEBUG) console.info('.... resolved', p)
+        if (DEBUG) debug('.. found: %s', p)
         return _withExtension(p)
       }
     },
