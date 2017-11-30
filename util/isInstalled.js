41b765469ced1e784bc7900462c89aad7e358088
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-11-30 16:02:43 +0100

    Tidy-up.

--- util/isInstalled.js
+++ util/isInstalled.js
@@ -2,7 +2,7 @@ var Module = require('module')
 
 module.exports = function isInstalled(moduleName, options = {}) {
   const rootDir = options.rootDir || process.cwd()
-    // install only if not yet installed
+  // install only if not yet installed
   // TODO: we should compare the version if given
   let lookupPaths = Module._nodeModulePaths(rootDir)
   let modulePath = Module._findPath(moduleName, lookupPaths, false)
