d7f539307860047e118ed25a789c96322cfedb3f
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-05-11 00:49:08 +0200

    Introduce rollup extension.

---
+++ extensions/rollup.js
@@ -0,0 +1,49 @@
+module.exports = function _rollup (b, config) {
+  const rollup = require('rollup')
+  let inputOptions = config
+  let outputOptions = config.output
+  let msg = `Rollup: ${config.input}`
+  b.custom(msg, {
+    src: null,
+    dest: null,
+    execute () {
+      if (b.opts.watch) {
+        return new Promise((resolve, reject) => {
+          let watcher = rollup.watch(config)
+          let initial = true
+          let t0
+          watcher.on('event', event => {
+            switch (event.code) {
+              case 'START': {
+                if (!initial) {
+                  console.log(`Rollup: ${config.input}`)
+                  t0 = Date.now()
+                }
+                break
+              }
+              case 'END': {
+                if (initial) {
+                  initial = false
+                  resolve()
+                } else {
+                  console.log('Finished in %s ms', Date.now() - t0)
+                }
+                break
+              }
+              default:
+                //
+            }
+          })
+        })
+      } else {
+        return rollup.rollup(inputOptions).then(bundle => {
+          if (Array.isArray(outputOptions)) {
+            return Promise.all(outputOptions.map(output => bundle.write(output)))
+          } else {
+            return bundle.write(outputOptions)
+          }
+        })
+      }
+    }
+  })
+}
