2554236bf324d16f58372834f8417f426f27856f
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-08-21 23:09:35 +0200

    Improve console output of webpack extension.

--- extensions/webpack.js
+++ extensions/webpack.js
@@ -1,3 +1,5 @@
+const { colors } = require('../dist/vendor')
+
 module.exports = function _webpack (b, config) {
   let webpack = require('webpack')
   let dest = config.output.path
@@ -6,21 +8,33 @@ module.exports = function _webpack (b, config) {
     dest,
     execute () {
       return new Promise((resolve, reject) => {
+        let _firstRun = true
         function _handler (err, stats) {
           if (err) {
             console.error(err.stack || err)
             reject(err)
           } else if (stats.hasErrors()) {
-            const info = stats.toJson()
+            const info = stats.toJson('normal')
             console.error(info.errors)
             reject(new Error(info.errors))
           } else {
-            console.log('Finished in %s ms', stats.endTime - stats.startTime)
+            let toStringOpts = { colors: true }
+            let ellapsedTime = stats.endTime - stats.startTime
+            console.log(stats.toString(toStringOpts))
+            if (_firstRun) {
+              _firstRun = false
+            } else {
+              console.log(colors.green(`..finished in ${ellapsedTime} ms`))
+            }
             resolve()
           }
         }
         if (b.opts.watch) {
-          webpack(config).watch({}, _handler)
+          let compiler = webpack(config)
+          compiler.watch({}, _handler)
+          compiler.hooks.watchRun.tap('bundler-plugin-webpack', () => {
+            console.log(`Webpack: ${dest}`)
+          })
         } else {
           webpack(config, _handler)
         }
