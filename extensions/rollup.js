0fd4151a3d27367a32e7ee001ee4b69546657e3c
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-08-24 00:33:57 +0200

    Allow for multi-entry rollup configurations.

--- extensions/rollup.js
+++ extensions/rollup.js
@@ -7,60 +7,72 @@
  * @param {object} config see [rollup documentation](https://rollupjs.org/guide/en/#big-list-of-options)
  */
 module.exports = function rollupBundlerExtension (b, config) {
-  const rollup = require('rollup')
-  let inputOptions = config
-  let outputOptions = config.output
-  let msg = `Rollup: ${config.input}`
-  b.custom(msg, {
+  let descr
+  if (Array.isArray(config)) {
+    descr = `Rollup: ${config.map(c => c.input).join(', ')}`
+  } else {
+    descr = `Rollup: ${config.input}`
+  }
+  b.custom(descr, {
     src: null,
     dest: null,
     execute () {
-      if (b.opts.watch) {
-        return new Promise((resolve, reject) => {
-          let watcher = rollup.watch(config)
-          let initial = true
-          let t0
-          watcher.on('event', event => {
-            switch (event.code) {
-              case 'START': {
-                if (!initial) {
-                  console.log(`Rollup: ${config.input}`)
-                  t0 = Date.now()
-                }
-                break
-              }
-              case 'END': {
-                if (initial) {
-                  initial = false
-                  resolve()
-                } else {
-                  console.log('Finished in %s ms', Date.now() - t0)
-                }
-                break
-              }
-              case 'ERROR':
-              case 'FATAL': {
-                if (initial) {
-                  reject(event.error)
-                } else {
-                  console.error(event.error)
-                }
-                break
-              }
-              default:
-                // console.log(`Unhandled event: ${event.code}`)
-            }
-          })
-        })
+      if (Array.isArray(config)) {
+        return Promise.all(config.map(c => _execute(b, c)))
       } else {
-        return rollup.rollup(inputOptions).then(bundle => {
-          if (Array.isArray(outputOptions)) {
-            return Promise.all(outputOptions.map(output => bundle.write(output)))
-          } else {
-            return bundle.write(outputOptions)
-          }
-        })
+        return _execute(b, config)
       }
     }
   })
+}
+
+function _execute (b, config) {
+  const rollup = require('rollup')
+  if (b.opts.watch) {
+    return new Promise((resolve, reject) => {
+      let watcher = rollup.watch(config)
+      let initial = true
+      let t0
+      watcher.on('event', event => {
+        switch (event.code) {
+          case 'START': {
+            if (!initial) {
+              console.log(`Rollup: ${config.input}`)
+              t0 = Date.now()
+            }
+            break
+          }
+          case 'END': {
+            if (initial) {
+              initial = false
+              resolve()
+            } else {
+              console.log('Finished in %s ms', Date.now() - t0)
+            }
+            break
+          }
+          case 'ERROR':
+          case 'FATAL': {
+            if (initial) {
+              reject(event.error)
+            } else {
+              console.error(event.error)
+            }
+            break
+          }
+          default:
+            // console.log(`Unhandled event: ${event.code}`)
+        }
+      })
+    })
+  } else {
+    return rollup.rollup(config).then(bundle => {
+      let outputOptions = config.output
+      if (Array.isArray(outputOptions)) {
+        return Promise.all(outputOptions.map(output => bundle.write(output)))
+      } else {
+        return bundle.write(outputOptions)
+      }
+    })
+  }
 }
